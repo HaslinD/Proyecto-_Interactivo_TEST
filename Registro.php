bfc86caa991baf808cd5788a0837cfe582e52b0f
HaslinD <haslind@unitec.edu>
2021-09-07 14:26:15 -0600

    First
    pruebas de proyecto

---
+++ Registro.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="css/style2.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>RedSocial</title>
+
+  </head>
+
+  <body>
+
+    <header>
+      <div class="container">
+
+        <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="index.html"><img src="images/Logo_UNITEC.png" alt="Logo Terceto" class="img-fluid" width="10%"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">  
+
+        </ul>
+
+        <ul class="navbar-nav ml-auto lista-redes">
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-facebook"></i></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-twitter"></i></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-instagram"></i></a>
+          </li>
+        </ul>
+        </div>
+      </nav>
+
+      </div>
+    </header>
+
+    <section id="Formulario">
+      <div class="container">
+
+        <!--<button class="open-button" onclick="openForm()">Open Form</button>-->
+
+        <div id="myForm">
+          <form method="post" action="Registro.php" class="form-container" enctype="multipart/form-data">
+            <h1>Nuevo Registro</h1>
+            <h6>Para registrarse recuerde que debe ser estudiante activo de UNITEC.</h6>
+            <label for="nombre"><b>Nombre</b></label>
+            <input type="text" placeholder="Ingrese nombre" name="nombre" required>
+
+            <label for="apellido"><b>Apellido</b></label>
+            <input type="text" placeholder="Ingrese apellido" name="apellido" required>
+
+            <label for="Email"><b>Numero de Cuenta</b></label>
+            <input type="text" placeholder="Ingrese su numero de cuenta" name="cuenta" required>
+
+            <label for="Usuario"><b>Numero de Telefono</b></label>
+            <input type="text" placeholder="Ingrese su numero Telefonico" name="numero" required>
+
+            <label for="Email"><b>Correo electrónico</b></label>
+            <input type="text" placeholder="Ingrese su Email" name="correo" required>
+
+            <label for="Contraseña"><b>Contraseña</b></label>
+            <input type="password" placeholder="Ingrese Contraseña" name="contra" required>
+
+            <button type="submit" name="enviar" value="submit" class="btn">REGISTRARSE</button>
+          </form>
+        </div>
+        
+
+      </div>
+    </section>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/FileSaver.js"></script>
+
+  </body>
+
+</html>
+
+<?php
+
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die("Error en Conexion");
+
+if (isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['cuenta']) && isset($_POST['numero']) && isset($_POST['correo']) && isset($_POST['contra'])) {
+
+  $Nombre = get_post($conn, 'nombre');
+  $Apellido = get_post($conn, 'apellido');
+  $NumCuenta = get_post($conn, 'cuenta');
+  $NumTelefono = get_post($conn, 'numero');
+  $Correo = get_post($conn, 'correo');
+  $Contraseña = get_post($conn, 'contra');
+
+  $query = "INSERT INTO usuarios (nombre, apellido, num_cuenta, num_telefono, correo, contrasenia)
+            VALUES" . "('$Nombre','$Apellido','$NumCuenta','$NumTelefono','$Correo','$Contraseña')";
+
+  $result = $conn->query($query);
+  if (!$result) echo "INSERT no se ha realizado<br><br>";
+
+
+  $nom_dir = "$Nombre"."_"."$Apellido"."_"."$NumCuenta";
+  mkdir("Users/"."$nom_dir", 0700);
+  
+  $conn->close();
+
+  function get_post($conn, $var) {
+    return $conn->real_escape_string($_POST[$var]);
+  }
+
+  if (isset($_POST['enviar'])){
+    ?>
+      <script type="text/javascript">
+        window.location = "index.php";
+      </script>
+    <?php
+  }
+}
+?>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>RedSocial</title>
+  </head>
+  
+  <body>
+  
+    <div class="content">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <img src="/images/Login-Arte.png" alt="Image" class="img-fluid" width="80%">
+          </div>
+          <div class="col-md-6 contents">
+            <div class="row justify-content-center">
+              <div class="col-md-8">
+                <img src="/images/Logo_UNITEC.png" alt="Image" class="img-fluid" width="50%">
+                <div class="mb-4">
+                <h3>Iniciar sesión</h3>
+                <p class="mb-4">Ingrese sus datos para continuar.</p>
+              </div>
+              <form action="index.php" method="post">
+                <div class="form-group first">
+                  <label for="username">Correo</label>
+                  <input type="text" class="form-control" id="username" name="user">
+
+                </div>
+                <div class="form-group last mb-4">
+                  <label for="password">Contraseña</label>
+                  <input type="password" class="form-control" id="password" name="pass">
+                  
+                </div>
+                
+                <div class="d-flex mb-5 align-items-center">
+                  <label class="control control--checkbox mb-0"><span class="caption">Recuerdame</span>
+                    <input type="checkbox" checked="checked"/>
+                    <div class="control__indicator"></div>
+                  </label>
+                  <span class="ml-auto"><a href="#" class="forgot-pass">Olvide mi contraseña</a></span> 
+                </div>
+                
+                <button type="submit" name="enviar" value="submit" class="btn btn-block btn-primary">CONTINUAR</button>
+
+                <?php
+                  if (isset($_POST['enviar'])) {
+                    require_once 'login.php';
+                    $conn = new mysqli($hn, $un, $pw, $db);
+                    if ($conn->connect_error) die("Error en Conexion");
+
+                    $usuario =  $_POST['user'];
+                    $contraseña =  $_POST['pass'];
+
+                    $query = "SELECT nombre, apellido, num_cuenta, num_telefono, correo, contrasenia FROM usuarios WHERE correo = '$usuario' && contrasenia = '$contraseña' ";
+                    $result = $conn->query($query);
+
+                    if (!$result) die("Fatal Error");
+
+                    session_start();
+                    $_SESSION['Nom'] = $row['nombre'];
+                    $_SESSION['Apel'] = $row['apellido'];
+                    $_SESSION['N_Cuenta'] = $row['num_cuenta'];
+                    $_SESSION['N_Tel'] = $row['num_telefono'];
+                    $_SESSION['Correo'] = $row['correo'];
+                    $_SESSION['Contra'] = $row['contrasenia'];
+
+                    $result->close();
+                    $conn->close();
+                    
+                    if (isset($_POST['enviar'])){
+                      ?>
+                        <script type="text/javascript">
+                          window.location = "main.html";
+                        </script>
+                      <?php
+                    }
+                  }
+                ?>
+
+                <div class="d-flex mb-5 align-items-center"> 
+                  <span class="ml-left"><a href="Registro.php" class="forgot-pass">Crear una cuenta</a></span>  
+                  <span class="ml-auto" ><a href="Registro.php" class="forgot-pass">Ingresar como invitado</a></span>
+                </div>
+                
+                </form>
+              </div>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+  
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+  
+</html>
