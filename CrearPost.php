b284aaf048429eaab4f9db348a8acd29cd08ffe5
LiHanlin <raptor43739@gmail.com>
2021-09-17 21:38:55 -0600

    Arreglando Post
    Funcion insertar a table y agregado de form

--- CrearPost.php
+++ CrearPost.php
@@ -1,21 +1,21 @@
 <?php
-
   require_once 'login.php';
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die("Error en Conexion");
 
-  if (isset($_POST['descripcion']) && isset($_POST['tag']) && $_FILES){
-   
+  if (isset($_POST['enviar'])){
     $perfil = $_FILES['perfil']['name'];
-    $descripcion = get_post($conn, 'descripcion');
-    $tag = get_post($conn, 'tag');
-   
+    $descripcion = $_POST['descripcion'];
+    $tag = $_POST['tag'];
+
+    echo $perfil.$descripcion.$tag;
+
     if ($perfil != "" || $descripcion != "") {
         session_start();
         $NombreUsuario = $_SESSION['Nom'];
         $NumeroCuenta = $_SESSION['N_Cuenta'];
         $id = $_SESSION['id'];
-        $date = date('Y-m-d H:i:s');
+        $date = Date('Y-m-d H:i:s.n');
 
         $query3 = "SELECT count(user_ID) AS count FROM post WHERE user_ID = '$id'";
         $result3 = $conn->query($query3);
@@ -23,7 +23,6 @@
         if (!$result3) die("Fatal Error");
 
         $row2 = $result3->fetch_array(MYSQLI_ASSOC);
-
         $count = $row2['count'];
 
         switch ($_FILES['perfil']['type']) {
@@ -42,24 +41,23 @@
         }
 
         if ($ext) {
-          $n  = $count.$ext;
+          $n  = $count.".".$ext;
           $destdir = 'Users/'.$NumeroCuenta.'/Posts/';
-          move_uploaded_file($_FILES['perfil']['tmp_name'], $destdir.$n);    
+          move_uploaded_file($_FILES['perfil']['tmp_name'], $destdir.$n);
 
         } else {
 
         }
-        
         $query2 = "SELECT ID FROM usuarios WHERE num_cuenta = '$NumeroCuenta' ";
         $result2 = $conn->query($query2);
 
         if (!$result2) die("Fatal Error");
 
         $row = $result2->fetch_array(MYSQLI_ASSOC);
-        $u = $row['ID']; 
+        $u = $row['ID'];
 
         $query = "INSERT INTO post (imagen, descripcion, tag, fecha, user_ID)
-                  VALUES" . "('$perfil','$descripcion','$tag','$date', $u)";
+                  VALUES" . "('$n','$descripcion','$tag','$date', $u)";
 
         $result = $conn->query($query);
         if (!$result) die("Fatal Error");
@@ -67,12 +65,11 @@
         $result->close();
         $result2->close();
         $conn->close();
+
     } else {
         echo "Error perro";
     }
-
   }
-
 ?>
 
 <!doctype html>
@@ -81,7 +78,7 @@
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <link href="css/CrearPerfil.css" rel="stylesheet">
@@ -121,8 +118,9 @@
         </header>
         <section id="CrearPost">
             <div class="container">
+              <form method="post" action="CrearPost.php" class="form-container" enctype="multipart/form-data">
                 <div class="row">
-                    <article class="col-md-6">
+                    <article class="col-md-8">
                         <label for="Perfil"><b>Subir Imagen</b></label>
                         <div class="imgUp">
                             <div class="imagePreview "></div>
@@ -131,7 +129,7 @@
                             </label>
                         </div>
                     </article>
-                    <article class="col-md-6">
+                    <article class="col-md-4">
                         <label for="descripcion"><b>Descripción del post</b></label>
                         <textarea name="descripcion" rows="5" cols="30"></textarea>
                         <label for="Tag"><b>Tag:</b></label>
@@ -141,9 +139,10 @@
                             Blender
                         </label>
                         <br><br>
-                        <button type="submit" name="enviar" value="submit" class="btn">Listo</button>
+                        <button type="submit" name="enviar" value="submit" class="btn btn-black">Listo</button>
                     </article>
                 </div>
+              </form>
             </div>
         </section>
     </body>
@@ -160,17 +159,17 @@
                 var uploadFile = $(this);
                 var files = !!this.files ? this.files : [];
                 if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
-        
+
                 if (/^image/.test( files[0].type)){ // only image file
                     var reader = new FileReader(); // instance of the FileReader
                     reader.readAsDataURL(files[0]); // read the local file
-        
+
                     reader.onloadend = function(){ // set image data as background of div
                         //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
                         uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");
                     }
                 }
-            
+
             });
         });
     </script>
